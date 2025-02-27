1fdb8cec123f27f2421ed1f73267e15a5ee07672
daniel <dannesart@live.com>
2025-02-27 22:52:28 +0100

    Headline component + more info on home page

--- components/Headline.vue
+++ components/Headline.vue
@@ -5,7 +5,13 @@
 
   <h2 v-if="size === 2" class="font-bebas text-8xl">
     <slot />
+    <div v-if="underline" class="h-2 w-20 bg-black"></div>
   </h2>
+
+  <h3 v-if="size === 3" class="font-antonio text-[2.5rem]">
+    <slot />
+    <div v-if="underline" class="h-1 w-10 bg-black"></div>
+  </h3>
 </template>
 
 <script setup lang="ts">
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <NuxtLayout>
-    <h1 class="font-bebas text-9xl mb-28">
+    <h1 class="font-bebas text-9xl mb-36">
       Creating experiences
       <span class="font-antonio text-8xl block mb-7"
         >For your stone age brand.</span
@@ -16,7 +16,11 @@
     </h1>
 
     <section>
-      <Headline :size="2">Offering</Headline>
+      <Headline :size="2" :underline="true">Offering</Headline>
+      <Headline :size="3"
+        >What our brave cavemen can offer you and your tribe</Headline
+      >
+      <div class="">Coming soon...</div>
     </section>
   </NuxtLayout>
 </template>
